5c056d8c68e0664d89adcdddbd8bc5744758ddc7
rjandre <rjandres@bfar.da.gov.ph>
2022-11-26 11:09:36 +0800

    feat : Post Model - added attributes

--- backend/models/postmodel.js
+++ backend/models/postmodel.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const PostsSchema = new Schema({
+    pets_id : {type : Schema.Types.ObjectId, ref : "pets", require : true},
+    comments : [{type : Schema.Types.ObjectId, ref : 'comments', default : []}],
+    reacts : [{type : Schema.Types.ObjectId, ref : 'reacts', default : []}],
+    body : {type : String, require : true},
+    date_created : {type : Date, deafaul : Date.now},
+    date_updated : {type : Date, deafaul : Date.now}
+})
+
+module.exports = mongoose.model('posts', PostsSchema)
