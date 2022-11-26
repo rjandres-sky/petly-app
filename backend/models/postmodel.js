45a1d3d2e8f6aaf6f61e41562435defcd7e14fe9
rjandre <rjandres@bfar.da.gov.ph>
2022-11-26 12:45:17 +0800

    feat : Post Router - added get, post, put, delete request

--- backend/models/postmodel.js
+++ backend/models/postmodel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const PostsSchema = new Schema({
-    pets_id : {type : Schema.Types.ObjectId, ref : "pets", require : true},
+    pet_id : {type : Schema.Types.ObjectId, ref : "pets", require : true},
     comments : [{type : Schema.Types.ObjectId, ref : 'comments', default : []}],
     reacts : [{type : Schema.Types.ObjectId, ref : 'reacts', default : []}],
     body : {type : String, require : true},
