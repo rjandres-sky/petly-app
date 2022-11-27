5594a4308499f69c81e739d528fda62193314aa9
AlfredBrandon <calawa.alfredbrandon@gmail.com>
2022-11-27 17:53:31 +0800

    feat: added news-feed route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,6 @@ import LandingPage from './pages/LandingPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import GetStartedPage from './pages/GetStartedPage';
-<<<<<<< HEAD
-import ProfilePage from './pages/ProfilePage';
-
-const App = () => {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element = { <LandingPage/>}/>
-        <Route path='/login' element = { <LoginPage/>}/>
-        <Route path='/register' element = { <RegisterPage/>}/>
-        <Route path='/get-started' element = { <GetStartedPage/>}/>
-        <Route path='/profile-page' element = { <ProfilePage/>}/>
-      </Routes>
-    </div>
-=======
 import NewsFeedPage from './pages/NewsFeedPage';
 
 const App = () => {
@@ -33,7 +18,6 @@ const App = () => {
       {/* <Route path='/profile' element={<ProfilePage />} /> */}
       <Route path='/news-feed' element={<NewsFeedPage />} />
     </Routes>
->>>>>>> c4edfefb55b3356e6b1e010557b3666dc2124cfc
   );
 }
 
