4b1f2524c4f365c85054ed106ceca805ad2aecad
rjandre <rjandres@bfar.da.gov.ph>
2022-11-30 13:07:27 +0800

    feat : add Post Filter

--- frontend/src/pages/NewsFeedPage.jsx
+++ frontend/src/pages/NewsFeedPage.jsx
@@ -14,28 +14,33 @@ const NewsFeedPage = () => {
     const currentUser = useSelector(state => state.Auth)
     const navigate = new useNavigate();
     const dispatch = new useDispatch()
-    
-    console.dir(allPost)
-    const [posts, setPosts] = useState([])
 
+    const [posts, setPosts] = useState('')
     const { data } = require('../data')
 
     const getAllPost = () => {
         axios.get('http://localhost:8080/all')
             .then(result => {
                 dispatch({
-                    type: "LOAD_ALLPOST", payload : result.data
-                }) 
+                    type: "LOAD_ALLPOST", payload: result.data
+                })
             }
             )
             .catch(console.log)
     }
 
-
+    const handleFilter = (name) => {
+        console.log(name)
+        if(name === 'all-pets'){
+            setPosts('')
+        } else {
+            setPosts(name)
+        }
+        
+    }
 
     useEffect(() => {
-        console.log(currentUser._id)
-        if(currentUser._id === undefined) {
+        if (currentUser._id === undefined) {
             navigate('/login')
         }
         getAllPost()
@@ -44,58 +49,59 @@ const NewsFeedPage = () => {
 
     return (
         <section className='container d-flex justify-content-center m-2 p-2 '>
-        <Navbar/>
-        <div className=' '>
-            {<div
-                className='pet-type-container'>
-                {
-                    data.map((items) => (
-                        <div
-                            className='pet-type'
-                            key={items.name}>
-                            {/* {console.log('icons', items.icon)} */}
+            <Navbar />
+            <div className=' '>
+                {<div
+                    className='pet-type-container'>
+                    {
+                        data.map((items) => (
                             <div
-                                className='pet-icon'>
-                                <img
-                                    src={items.icon}
-                                    alt='' />
+                                className='pet-type'
+                                key={items.name}>
+                                {/* {console.log('icons', items.icon)} */}
+                                <div
+                                    className='pet-icon'>
+                                    <img key={items.name}
+                                        onClick={() => handleFilter(items.name)}
+                                        src={items.icon}
+                                        alt='' />
+                                </div>
                             </div>
-                        </div>
-                    ))
-                }
-            </div>}
-            <div
-                className='pet-username-container'>
-                <div>
-                </div>
-                <div>
+                        ))
+                    }
+                </div>}
+                <div
+                    className='pet-username-container'>
+                    <div>
+                    </div>
+                    <div>
 
+                    </div>
                 </div>
-            </div>
-            <div
-                className='pet-post-container'>
-                    {allPost.map(post => {
+                <div
+                    className='pet-post-container'>
+                    {allPost.filter(pet => pet.pet_id.pet_type.includes( posts )).map(post => {
                         return (
-                            <PostComponent post = {post} />
+                            <PostComponent post={post} />
                         )
                     })}
-            </div>
-            <div
-                className='pet-reacts-container'>
-
-            </div>
-            <div
-                className='pet-caption-container'>
+                </div>
                 <div
-                    className='pet-caption-username'>
+                    className='pet-reacts-container'>
 
                 </div>
                 <div
-                    className='pet-caption'>
+                    className='pet-caption-container'>
+                    <div
+                        className='pet-caption-username'>
 
+                    </div>
+                    <div
+                        className='pet-caption'>
+
+                    </div>
                 </div>
             </div>
-        </div>
         </section>
     )
 }
