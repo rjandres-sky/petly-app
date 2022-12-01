3c5f8890c3e3464493f82d273a59d80044a70ec9
rjandre <rjandres@bfar.da.gov.ph>
2022-12-01 10:03:33 +0800

    fixt : adding commnent

--- backend/routers/routerComments.js
+++ backend/routers/routerComments.js
@@ -1,5 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const mongoose = require('mongoose')
+const uuidv4 = require('uuid/v4')
+const DIR = './uploads/';
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, DIR);
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, uuidv4() + '-' + fileName)
+    }
+});
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
+});
 
 
 const Posts = require('../models/postmodel')
@@ -20,7 +44,7 @@ router.get('/:id', (request, response) => {
 })
 
 //==================adding comment========================
-router.post('/post', async (request, response, next) => {
+router.post('/post', upload.single('img'), async (request, response, next) => {
     console.log(request.body)
     const newComment = {
         pet_id: request.body.pet_id,
